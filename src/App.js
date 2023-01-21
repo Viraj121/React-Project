b552aa9ecaa05faba565b1d19c11ec4919a49bbf
“Viraj <gavasviraj@gmail.com>
2023-01-21 16:19:34 +0530

    tailwindcss

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './component/Header'
+import Hero from './component/Hero';
+import Footer from './component/Footer'
+import Dark from './component/Dark';
+function App() {
+  return (
+    <>
+     <Header/>
+
+     <Hero/>
+
+     <Footer/>
+     {/* <Dark/> */}
+        </>
+  );
+}
+
+export default App;
